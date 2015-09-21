8a355bedd3d625b464ebc901c9cf092a867bae85
Costel Radulescu <wizard.ro@gmail.com>
2015-09-22 00:40:47 +0300

    updated App text and added more placeholders

--- src/main/java/org/costa/App.java
+++ src/main/java/org/costa/App.java
@@ -15,82 +15,67 @@ public class App {
   }
 
   public static void main(final String [] args) {
-    final String text =
-        "$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ "
-        + "asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ "
-        + "asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas "
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "$$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ "
-        + "agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da"
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ "
-        + "asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ "
-        + "asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas "
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "$$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ "
-        + "agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da"
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ "
-        + "asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ "
-        + "asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas "
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "$$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ "
-        + "agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da"
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ "
-        + "asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ "
-        + "asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas "
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "$$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ "
-        + "agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da"
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ "
-        + "asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ "
-        + "asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas "
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "$$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ "
-        + "agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da"
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ "
-        + "asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ "
-        + "asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas "
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ "
-        + "$$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da$$a$$ "
-        + "agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ $$abcdefgh$$ asdsda "
-        + "ads$$a$$sadas as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa "
-        + "$$xyeza$$ $$abcdefgh$$ asdsda ads$$a$$sadas as$$abcdefgh$$das da"
-        + "as$$abcdefgh$$das da$$a$$ agf$$a$$ag $$ab$$ asd$$a$sa $$xyeza$$ ";
+    final String text = "random textarandom text with no placeholdersshort "
+        + "$$a$$ text short $$a$$ text sh$$or$$t $$a$$ t$$e$$x$$t"
+        + "$$a$$ sh$$pattern$$t $$a$$ t$$e$$x$$t$$pattern$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t sh$$or$$t $$a$$ t$$e$$x$$t$$a$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t$$pattern$$ sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$$aaaaa$$sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$aaaaa$$sh$$or$$$$aaaaa$$$$t $$a$$ t$$e$$x$$t$$$$pattern$$$$"
+        + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$aa$$sh$$oaa$$sh$$oaa$$sh$$oaa$$sh$$o"
+        + "$$a$$ text short $$a$$ text sh$$or$$t $$a$$ t$$e$$x$$t"
+        + "$$a$$ sh$$pattern$$t $$a$$ t$$e$$x$$t$$pattern$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t sh$$or$$t $$a$$ t$$e$$x$$t$$a$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t$$pattern$$ sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$$aaaaa$$sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$aaaaa$$sh$$or$$$$aaaaa$$$$t $$a$$ t$$e$$x$$t$$$$pattern$$$$"
+        + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$aa$$sh$$oaa$$sh$$oaa$$sh$$oaa$$sh$$o"
+        + "$$a$$ text short $$a$$ text sh$$or$$t $$a$$ t$$e$$x$$t"
+        + "$$a$$ sh$$pattern$$t $$a$$ t$$e$$x$$t$$pattern$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t sh$$or$$t $$a$$ t$$e$$x$$t$$a$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t$$pattern$$ sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$$aaaaa$$sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$aaaaa$$sh$$or$$$$aaaaa$$$$t $$a$$ t$$e$$x$$t$$$$pattern$$$$"
+        + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$aa$$sh$$oaa$$sh$$oaa$$sh$$oaa$$sh$$o"
+        + "$$a$$ text short $$a$$ text sh$$or$$t $$a$$ t$$e$$x$$t"
+        + "$$a$$ sh$$pattern$$t $$a$$ t$$e$$x$$t$$pattern$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t sh$$or$$t $$a$$ t$$e$$x$$t$$a$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t$$pattern$$ sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$$aaaaa$$sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$aaaaa$$sh$$or$$$$aaaaa$$$$t $$a$$ t$$e$$x$$t$$$$pattern$$$$"
+        + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$aa$$sh$$oaa$$sh$$oaa$$sh$$oaa$$sh$$o"
+        + "$$a$$ text short $$a$$ text sh$$or$$t $$a$$ t$$e$$x$$t"
+        + "$$a$$ sh$$pattern$$t $$a$$ t$$e$$x$$t$$pattern$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t sh$$or$$t $$a$$ t$$e$$x$$t$$a$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t$$pattern$$ sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$$aaaaa$$sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$aaaaa$$sh$$or$$$$aaaaa$$$$t $$a$$ t$$e$$x$$t$$$$pattern$$$$"
+        + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$aa$$sh$$oaa$$sh$$oaa$$sh$$oaa$$sh$$o"
+        + "$$a$$ text short $$a$$ text sh$$or$$t $$a$$ t$$e$$x$$t"
+        + "$$a$$ sh$$pattern$$t $$a$$ t$$e$$x$$t$$pattern$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t sh$$or$$t $$a$$ t$$e$$x$$t$$a$$sh$$or$$t $$a$$ "
+        + "t$$e$$x$$t$$pattern$$ sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$$aaaaa$$sh$$or$$aaaaa$$t $$a$$ t$$e$$x$$t$$pattern$$"
+        + "$$$$aaaaa$$sh$$or$$$$aaaaa$$$$t $$a$$ t$$e$$x$$t$$$$pattern$$$$"
+        + "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$aa$$sh$$oaa$$sh$$oaa$$sh$$oaa$$sh$$o";
     App p = new App();
     p.placeholderMap = new PlaceholderMapImpl();
     p.placeholderMap.addPlaceholderValue("$$a$$", "first_value");
+    p.placeholderMap.addPlaceholderValue("$$aa$$", "aa_value");
     p.placeholderMap.addPlaceholderValue("$$ab$$", "second_value");
+    p.placeholderMap.addPlaceholderValue("$$or$$", "or_value");
     p.placeholderMap.addPlaceholderValue("$$xyeza$$", "third_value");
     p.placeholderMap.addPlaceholderValue("$$abcdefgh$$", "fourth_value");
+    p.placeholderMap.addPlaceholderValue("$$pattern$$", "fifth_value");
+    p.placeholderMap.addPlaceholderValue("$$aaaaa$$", "sixth_value");
     p.setPlaceholderReplacementStrategy(
-        PlaceholderReplacementStrategies.LINEAR.getStrategy());
+        PlaceholderReplacementStrategies.DEFAULT.getStrategy());
     long start = System.nanoTime();
     String result1 = p.replace(text);
     long end = System.nanoTime();
     System.out.println(end - start);
     p.setPlaceholderReplacementStrategy(
-        PlaceholderReplacementStrategies.DEFAULT.getStrategy());
+        PlaceholderReplacementStrategies.LINEAR.getStrategy());
     start = System.nanoTime();
     String result = p.replace(text);
     end = System.nanoTime();
